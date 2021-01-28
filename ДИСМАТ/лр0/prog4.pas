1d2f54b1aec3eff8efa643f555a0cb39e68a5282
Dmitry Mashurov <lagrik33@gmail.com>
2021-01-28 20:45:00 +0300

    added DISMAT folder with laboratory works

---
+++ ДИСМАТ/лр0/prog4.pas
@@ -0,0 +1,45 @@
+﻿type t_in_arr = array[1..10] of integer;
+     t_out_arr = array[1..20] of integer;
+
+procedure read_arr(var arr: t_in_arr);
+var i: integer;
+begin
+  for i := 1 to 10 do
+    read(arr[i]);
+end;
+
+procedure print_arr(var arr: t_out_arr);
+var i: integer;
+begin
+  for i := 1 to 20 do
+    write(arr[i],' ');  
+end;
+
+function is_elem_in(arr: t_in_arr; elem: integer): boolean;
+var i: integer;
+begin
+  is_elem_in := false;
+  
+  for i := 1 to 10 do
+    if (elem = arr[i]) then
+      is_elem_in := true;  
+end;
+
+var a,b: t_in_arr;
+    c: t_out_arr;
+    i, j: integer;
+  
+begin
+  read_arr(a);
+  read_arr(b);
+  
+  for i := 1 to 10 do
+    if not(is_elem_in(b,a[i])) then
+      c[i] := a[i];
+    
+  for i := 1 to 10 do
+    if not(is_elem_in(a,b[i])) then
+      c[i+10] := b[i];
+  
+  print_arr(c);
+end.
---
+++ ДИСМАТ/лр0/prog6.pas
@@ -0,0 +1,36 @@
+﻿type t_in_arr = array[1..10] of integer;
+
+procedure read_arr(var arr: t_in_arr);
+var i: integer;
+begin
+  for i := 1 to 10 do
+    read(arr[i]);
+end;
+
+function is_elem_in(arr: t_in_arr; elem: integer): boolean;
+var i: integer;
+begin
+  is_elem_in := false;
+  
+  for i := 1 to 10 do
+    if (elem = arr[i]) then
+      is_elem_in := true;  
+end;
+
+var a,b: t_in_arr;
+    i, j: integer;
+    f: boolean = true;
+  
+begin
+  read_arr(a);
+  read_arr(b);
+  
+  for i := 1 to 10 do
+    if not(is_elem_in(b,a[i])) and not(is_elem_in(a,b[i])) and (f = true) then
+      f := false;
+    
+  if (f = false) then
+    writeln('Неверно')
+  else
+    writeln('Верно');
+end.
