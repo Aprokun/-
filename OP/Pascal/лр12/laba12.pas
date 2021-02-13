bcd253b1aa42ff7f6fb974446c89da9e79f133a1
Dmitry Mashurov <lagrik33@gmail.com>
2021-02-13 23:27:29 +0300

    repaired laba12, pascal, op

---
+++ OP/Pascal/лр12/laba12.pas
@@ -0,0 +1,75 @@
+﻿const M = 4;
+      N = 4;
+
+type t_row = array[1..N] of integer;
+     t_p_row = ^t_row;
+     t_matr = array[1..M] of t_p_row;
+
+procedure create_matr(var a: t_matr);
+var i: byte;
+begin
+  for i := 1 to M do
+    New(a[i]);
+end;
+
+procedure read_matr(a: t_matr);
+var i,j: byte;
+begin
+  for i := 1 to M do
+    for j := 1 to N do
+      read(a[i]^[j]);
+end;
+
+procedure write_matr(a: t_matr);
+var i,j: byte;
+begin
+  for i := 1 to M do
+    begin
+      for j := 1 to N do
+        write(a[i]^[j],' ');
+    writeln();
+    end;
+end;
+
+procedure sort(var a: t_matr);
+var i,j: byte;
+    key: t_p_row;
+begin
+  for i := 2 to M do
+    begin
+      key := a[i];
+      j := i;
+      while (j > 1) and (a[j-1]^[1] < key^[1]) do
+        begin
+          a[j] := a[j-1];
+          j := j - 1;
+        end;
+      a[j] := key;
+    end;
+end;
+
+procedure del_matr(var a: t_matr);
+var i: byte;
+begin
+  for i := 1 to M do
+    Dispose(a[i]);
+end;
+
+var a: t_matr;
+    l: integer;
+
+begin
+create_matr(a);
+
+writeln('Ввод матрицы: ');
+read_matr(a);
+
+sort(a);
+
+writeln('Вывод отсортированной матрицы: ');
+write_matr(a);
+
+del_matr(a);
+
+read(l);
+end.
---
+++ OP/Pascal/лр12/laba123.pas
@@ -0,0 +1,86 @@
+{$CODEPAGE UTF-8}
+
+const MAX = 20000;
+
+type t_max_row = array[1..MAX div SizeOf(integer)] of integer;
+     t_p_max_row = ^t_max_row;
+     t_max_matr = array[1..MAX div SizeOf(pointer)] of t_p_max_row;
+     t_p_max_matr = ^t_max_matr;
+
+procedure create_matr(var pa: t_p_max_matr; m,n: byte);
+var i: byte;
+begin
+  GetMem(pa,m*SizeOf(pointer));
+  for i := 1 to m do
+    GetMem(pa^[i],n*SizeOf(integer));
+end;
+
+procedure read_matr(pa: t_p_max_matr; m,n: byte);
+var i,j: byte;
+begin
+  for i := 1 to m do
+    for j := 1 to n do
+      read(pa^[i]^[j]);
+end;
+
+procedure write_matr(pa: t_p_max_matr; m,n: byte);
+var i,j: byte;
+begin
+  for i := 1 to m do
+    begin
+      for j := 1 to n do
+        write(pa^[i]^[j],' ');
+    writeln();
+    end;
+end;
+
+procedure sort(var pa: t_p_max_matr; m: byte);
+var i,j: byte;
+    key: t_p_max_row;
+begin
+  for i := 2 to m do
+    begin
+      key := pa^[i];
+      j := i;
+      while (j > 1) and (pa^[j-1]^[1] < key^[1]) do
+        begin
+          pa^[j] := pa^[j-1];
+          j := j - 1;
+        end;
+      pa^[j] := key;
+    end;
+
+end;
+
+procedure del_matr(var pa: t_p_max_matr; m,n: byte);
+var i: byte;
+begin
+  for i := 1 to m do
+    FreeMem(pa^[i],n*SizeOf(integer));
+  FreeMem(pa,m*SizeOf(pointer));
+end;
+
+var a: t_p_max_matr;
+    l,m,n: integer;
+
+begin
+writeln('Введите кол-во строк');
+readln(m);
+
+writeln('Введите кол-во элементов в строке');
+readln(n);
+
+create_matr(a,m,n);
+
+writeln('Ввод матрицы: ');
+read_matr(a,m,n);
+
+sort(a,m);
+
+writeln('Вывод отсортированной матрицы: ');
+write_matr(a,m,n);
+
+del_matr(a,m,n);
+
+read(l);
+end.
