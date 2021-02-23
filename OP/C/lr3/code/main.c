c708858a0dbd6635d8b31923265c06f048c9b199
Dmitry Mashurov <lagrik33@gmail.com>
2021-02-23 21:37:36 +0300

    laba3 op, laba3-1 physics, laba1 n laba2 dismat

--- OP/C/lr3/code/main.c
+++ OP/C/lr3/code/main.c
@@ -1,80 +1,84 @@
+///Удалить в шестнадцатеричной записи данного целого числа четные цифры.
+
 #include <stdio.h>
+#include <stdlib.h>
 
-int to_decimal(int hex_num)
+static char hex_digits[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+int rev_num(int num)
 {
-    int dec_num = 0, c;
-
-    while (hex_num > 0)
-    {
-        switch (hex_num % 16)
-        {
-            case 'F':
-                c = 15;
-                break;
-            case 'E':
-                c = 14;
-                break;
-            case 'D':
-                c = 13;
-                break;
-            case 'C':
-                c = 12;
-                break;
-            case 'B':
-                c = 11;
-                break;
-            case 'A':
-                c = 10;
-                break;
-            default:
-                c = hex_num % 16;
-                break;
-        }
-
-        s
-    }
-}
-
-int reverse_num(int num)
-{
-    int reversed_num;
+    int res = 0;
 
     while (num > 0)
     {
-        reversed_num = reversed_num * 10 + num % 10;
+        res = num * 10 + num % 10;
         num /= 10;
     }
 
-    return reversed_num;
+    return res;
 }
 
-int to_binary(int num)
+int dec_to_bin(int num)
 {
-    int bin_num = 0;
+    int res = 0;
 
     while (num > 0)
     {
-        bin_num = bin_num * 10 + num % 2;
+        res = 10 * res + num % 2;
         num /= 2;
     }
 
-    return reverse_num(bin_num);
+    res = rev_num(res);
+
+    return res;
 }
 
-static int even_nums[] = {2, 4, 6, 8, 10, 12, 14, 16};
+int pow(int num, size_t degree)
+{
+   int res = 1;
+
+   for (size_t i = 0; i < degree; i++)
+   {
+       res *= num;
+   }
+
+    return res;
+}
+
+int dec_to_hex_wtht_evns(int dec_num)
+{
+    int dig, i = 0, hex_num_in_dec = 0;
+
+    while (dec_num > 0)
+    {
+        dig = dec_num % 16;
+
+        if (dig % 2 != 0)
+        {
+            hex_num_in_dec += dig * pow(16, i);
+            ++i;
+        }
+
+        dec_num /= 16;
+    }
+
+    return hex_num_in_dec;
+}
 
 int main()
 {
-    printf("Input hexadecimal num:\n");
+    printf("Input decimal num:\n");
     int num;
-    scanf("%x", &num);
+    scanf("%i", &num);
 
-    printf("%X",num % 16);
-
-    int bin_num = toBinary(num);
-    printf("Inputted num in binary: %i", bin_num);
+    int bin_num = dec_to_bin(num);
+    printf("Inputted dec num in binary: %i", bin_num);
 
 
+    int hex_num_in_dec = dec_to_hex_wtht_evns(num);
+    int hex_num_in_bin = dec_to_bin(hex_num_in_dec);
 
-    printf("Inputted hex num without odds%X",num);
+    printf("Inputted decimal num in hexadecimal: %X\n",num);
+    printf("Inputted decimal num in hexadecimal without evens: %X\n",hex_num_in_dec);
+    printf("Inputted decimal num in hexadecimal without evens in bin: %i", hex_num_in_bin);
 }
