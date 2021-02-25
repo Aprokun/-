3d85afac91f5be4cf0f611de18d5d53e58fe9963
Dmitry Mashurov <lagrik33@gmail.com>
2021-02-25 09:25:29 +0300

    laba3 op

--- OP/C/lr3/code/main.c
+++ OP/C/lr3/code/main.c
@@ -2,35 +2,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static char hex_digits[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
-int rev_num(int num)
+void rev_str(char str[], size_t len)
 {
-    int res = 0;
+    size_t i = 0, j = len - 1;
 
-    while (num > 0)
+    while (i < j)
     {
-        res = num * 10 + num % 10;
-        num /= 10;
+        char t = str[i];
+        str[i] = str[j];
+        str[j] = t;
+        i++; j--;
     }
-
-    return res;
-}
-
-int dec_to_bin(int num)
-{
-    int res = 0;
-
-    while (num > 0)
-    {
-        res = 10 * res + num % 2;
-        num /= 2;
-    }
-
-    res = rev_num(res);
-
-    return res;
 }
 
 int pow(int num, size_t degree)
@@ -45,6 +31,31 @@ int pow(int num, size_t degree)
     return res;
 }
 
+void print_dec_in_bin(int dec_num)
+{
+    char res[255];
+    size_t i = 0;
+
+    while (dec_num > 0)
+    {
+        if (dec_num % 2 == 0)
+        {
+            res[i] = '0';
+            ++i;
+        }
+        else
+        {
+            res[i] = '1';
+            ++i;
+        }
+
+        dec_num /= 2;
+    }
+
+    rev_str(res,i-1);
+    printf("%s\n",res);
+}
+
 int dec_to_hex_wtht_evns(int dec_num)
 {
     int dig, i = 0, hex_num_in_dec = 0;
@@ -53,7 +64,7 @@ int dec_to_hex_wtht_evns(int dec_num)
     {
         dig = dec_num % 16;
 
-        if (dig % 2 != 0)
+        if ((dig % 2 != 0) || (dig == 0))
         {
             hex_num_in_dec += dig * pow(16, i);
             ++i;
@@ -71,14 +82,11 @@ int main()
     int num;
     scanf("%i", &num);
 
-    int bin_num = dec_to_bin(num);
-    printf("Inputted dec num in binary: %i", bin_num);
-
+    print_dec_in_bin(num);
 
     int hex_num_in_dec = dec_to_hex_wtht_evns(num);
-    int hex_num_in_bin = dec_to_bin(hex_num_in_dec);
 
     printf("Inputted decimal num in hexadecimal: %X\n",num);
     printf("Inputted decimal num in hexadecimal without evens: %X\n",hex_num_in_dec);
-    printf("Inputted decimal num in hexadecimal without evens in bin: %i", hex_num_in_bin);
+    print_dec_in_bin(hex_num_in_dec);
 }
