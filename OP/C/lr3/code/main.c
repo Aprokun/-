9269fdb697c312c2f88d7d991f6ff8d94b1da455
Dmitry Mashurov <lagrik33@gmail.com>
2021-03-06 12:09:26 +0300

    push to computer

--- OP/C/lr3/code/main.c
+++ OP/C/lr3/code/main.c
@@ -4,51 +4,49 @@
 #include <stdlib.h>
 
 //вывод беззнакового числа num в двоичном представлении.
-void print_int_in_bin_u(unsigned int num)
+void print_bin_u(unsigned int num)
 {
     if (num)
     {
-        print_int_in_bin_u(num >> 1);
+        print_bin_u(num >> 1);
         printf("%i",num & 1);
     }
 }
 
 //вывод числа num в двоичном представлении.
-void print_int_in_bin(int num)
+void print_bin(int num)
 {
     if (num < 0)
     {
         putchar('-');
-        print_int_in_bin_u(- (unsigned int) num);
+        print_bin_u(-num);
     }
     else if (num > 0)
     {
-        print_int_in_bin_u(num);
+        print_bin_u(num);
     }
     else
-    {
-        putchar('0');
-    }
+        printf("0");
 }
 
-// возвращает преобразованное десятичное число dec_num,
+// возвращает преобразованное десятичное число num,
 // в шестнадцатеричной записи которого отсутствуют чётные цифры.
-int dec_to_hex_wtht_evns(int dec_num)
+int hex_without_evens(int num)
 {
     size_t i = 0;
     int hex_num_in_dec = 0,
-        sym = 1,
+        sign = 1,
         dig;
 
-    if (dec_num < 0)
+    if (num < 0)
     {
-        sym = -1;
-        dec_num *= sym;
+        sign = -1;
+        num = -num;
     }
 
-    while (dec_num > 0)
+    while (num > 0)
     {
-        dig = dec_num % 16;
+        dig = num % 16;
 
         if ((dig & 1) == 1)
         {
@@ -56,10 +54,10 @@ int dec_to_hex_wtht_evns(int dec_num)
             ++i;
         }
 
-        dec_num >>= 4;
+        num >>= 4;
     }
 
-    hex_num_in_dec *= sym;
+    hex_num_in_dec *= sign;
 
     return hex_num_in_dec;
 }
@@ -70,23 +68,23 @@ int main()
     int num;
     scanf("%i",&num);
     printf("Inputted num in bin\n");
-    print_int_in_bin(num);
+    print_bin(num);
     printf("\n");
 
-    int hex_num_in_dec = dec_to_hex_wtht_evns(num);
+    int new_num = hex_without_evens(num);
     printf("Inputted num in hex:\n");
 
     if (num < 0)
     {
         printf("-%X\n", -num);
-        printf("Inputted num in hex without even: -%X\n", -hex_num_in_dec);
+        printf("Inputted num in hex without even: -%X\n", -new_num);
     }
-    else if (num > 0)
+    else if (num >= 0)
     {
         printf("%X\n", num);
-        printf("Inputted num in hex without even: %X\n", hex_num_in_dec);
+        printf("Inputted num in hex without even: %X\n", new_num);
     }
 
     printf("Inputted num in hex without even in bin:\n");
-    print_int_in_bin(hex_num_in_dec);
+    print_bin(new_num);
 }
